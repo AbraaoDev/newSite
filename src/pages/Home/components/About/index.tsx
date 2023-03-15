098ad52a2637dbab1b8a0974fe46ef7cd938aa6b
Daniel Nora <daniel_nora@hotmail.com>
2023-03-15 19:37:10 -0300

    Fix

--- src/pages/Home/components/About/index.tsx
+++ src/pages/Home/components/About/index.tsx
@@ -1,55 +1,61 @@
-import { AboutContainer, AboutContent, FooterAbout, ImageSide, SocialButtons, TextSide } from "./styles";
-import personalImg from '../../../../assets/personalImage.svg'
-import helloAll from '../../../../assets/helloAll.svg'
+import {
+  AboutContainer,
+  AboutContent,
+  FooterAbout,
+  ImageSide,
+  SocialButtons,
+  TextSide,
+} from "./styles";
+import personalImg from "../../../../assets/personalImage.svg";
+import helloAll from "../../../../assets/helloAll.svg";
 import { GitlabLogoSimple, InstagramLogo, LinkedinLogo } from "phosphor-react";
 
-
-export function About(){
-    return(
-        <AboutContainer>
-            <div className="container">
-                <p><span>Um pouco sobre mim</span></p>
-
-                <AboutContent>
-                    <ImageSide>
-                        <img src={personalImg} />
-                    </ImageSide>
-                    <TextSide>
-                        <div>
-                            <h3>Olá, prazer</h3>
-                            <img src={helloAll} />
-                        </div>
-                        <aside>
-                            <h1>Abraão Moreira</h1>
-                            <p>Desenvolvedor FullStack</p>
-                            <span>
-                                Sou estudante de Ciências da Computação pela Universidade 
-                                Federal da Paraíba, com um forte interesse e paixão pela tecnologia. 
-                                Desenvolvedor Front-End e Designer UI/UX, sempre buscando evoluir e 
-                                se aprimorar na área de TI. 
-                            </span>
-                        </aside>
-                        
-
-                        <FooterAbout>
-                            <SocialButtons>
-                                <button>
-                                    <GitlabLogoSimple size={22} weight="fill" />
-                                </button>
-                                <button>
-                                    <InstagramLogo size={22} weight="fill" />
-                                </button>
-                                
-                            </SocialButtons>
-
-                            <div className="divisor"></div>
-
-                            <span><strong>Compartilho</strong> todo<br></br>
-                            o meu conteúdo aqui</span>
-                        </FooterAbout>
-                    </TextSide>
-                </AboutContent>
+export function About() {
+  return (
+    <AboutContainer>
+      <p className="section-divider">
+        <span>Um pouco sobre mim</span>
+      </p>
+      <div className="container">
+        <AboutContent>
+          <ImageSide>
+            <img src={personalImg} />
+          </ImageSide>
+          <TextSide>
+            <div>
+              <h3>Olá, prazer</h3>
+              <img src={helloAll} />
             </div>
-        </AboutContainer>
-    )
+            <aside>
+              <h1>Abraão Moreira</h1>
+              <p>Desenvolvedor FullStack</p>
+              <span>
+                Sou estudante de Ciências da Computação pela Universidade
+                Federal da Paraíba, com um forte interesse e paixão pela
+                tecnologia. Desenvolvedor Front-End e Designer UI/UX, sempre
+                buscando evoluir e se aprimorar na área de TI.
+              </span>
+            </aside>
+
+            <FooterAbout>
+              <SocialButtons>
+                <button>
+                  <GitlabLogoSimple size={22} weight="fill" />
+                </button>
+                <button>
+                  <InstagramLogo size={22} weight="fill" />
+                </button>
+              </SocialButtons>
+
+              <div className="divisor"></div>
+
+              <span>
+                <strong>Compartilho</strong> todo<br></br>o meu conteúdo aqui
+              </span>
+            </FooterAbout>
+          </TextSide>
+        </AboutContent>
+      </div>
+    </AboutContainer>
+  );
 }
--- src/pages/Home/components/About/styles.ts
+++ src/pages/Home/components/About/styles.ts
@@ -1,144 +1,109 @@
 import styled from "styled-components";
 
 export const AboutContainer = styled.div`
-    width: 100%;
-    height: 25rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  margin-top: 10rem;
+  .section-divider {
+    background: linear-gradient(#1f1f1f, #1f1f1f);
+    background-size: 100% 1px;
+    background-repeat: no-repeat;
+    background-position: center;
+    text-indent: 10%;
+    font-size: 0.875rem;
+    color: ${({ theme }) => theme.colors["white-span"]};
 
-    
-
-    div{
-        position: absolute;
-
-        > p{
-            background: linear-gradient(#1f1f1f, #1f1f1f);
-            background-size: 100% 1px;
-            background-repeat: no-repeat;
-            background-position: center;
-            text-indent: 10%;
-
-            font-size: 0.875rem;
-            color: ${({theme }) => theme.colors["white-span"]};
-            position: relative;
-
-            span{
-                background: ${({theme}) => theme.colors["base-black"]};
-                padding-inline: .5rem;
-            }
-
-            
-
-        
-            
-        }
-   
+    span {
+      background: ${({ theme }) => theme.colors["base-black"]};
+      padding-inline: 0.75em;
     }
-`
+  }
+`;
 
 export const AboutContent = styled.div`
-    display: flex;
-    justify-content: space-between;
-    gap: 7rem;
-`
+  display: flex;
+  justify-content: space-between;
+  gap: 7rem;
+`;
 
 export const ImageSide = styled.section`
-    margin-top: 5rem;
-    display: flex;
-`
+  margin-block: 5rem;
+  display: flex;
+`;
 
 export const TextSide = styled.section`
-    margin-top: 7rem;
+  margin-top: 7rem;
 
-    div{
-        display: flex;
-        align-items: center;
-        gap: 0.8rem;
+  div {
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
 
+    h3 {
+      font-weight: 400;
+      font-size: 1.125rem;
+      line-height: 1.375rem;
+    }
+  }
 
-        h3{
-            font-weight: 400;
-            font-size: 1.125rem;
-            line-height: 1.375rem;
-        }
+  aside {
+    margin-top: 3.125rem;
+
+    h1 {
+      font-size: 2.75rem;
+      font-weight: 700;
+      line-height: 3.125rem;
     }
 
-    aside{
-        margin-top: 3.125rem;
-
-        h1{
-            font-size: 2.75rem;
-            font-weight: 700;
-            line-height: 3.125rem;
-        }
-
-        p{
-            font-size: 1.125rem;
-            font-weight: 500;
-            line-height: 3.125rem;
-            margin-bottom: 2rem;
-            color: ${({theme}) => theme.colors["second-white"]};
-        }
-
-        span{
-            
-
-            font-size: 1.125rem;
-            font-weight: 400;
-            line-height: 1.625rem;
-            color: rgba(255, 255, 255, 0.65);
-        }
+    p {
+      font-size: 1.125rem;
+      font-weight: 500;
+      line-height: 3.125rem;
+      margin-bottom: 2rem;
+      color: ${({ theme }) => theme.colors["second-white"]};
     }
 
-`
-
+    span {
+      font-size: 1.125rem;
+      font-weight: 400;
+      line-height: 1.625rem;
+      color: rgba(255, 255, 255, 0.65);
+    }
+  }
+`;
 
 export const FooterAbout = styled.div`
-    margin-top: 5rem;
-    display: flex;
-    align-items: center;
-    
-    color: rgba(255, 255, 255, 0.65);
+  margin-top: 5rem;
+  display: flex;
+  align-items: center;
 
-    .divisor{
-        content: "";
-        height: 2.3rem;
-        border-right: 2px solid rgba(255, 255, 255, 0.65);
-        margin-left: 9rem;
-    }
+  color: rgba(255, 255, 255, 0.65);
 
-    span{
-        margin-left: 10rem;
-        font-weight: 300;
-    }
+  .divisor {
+    content: "";
+    height: 2.3rem;
+    border-right: 2px solid rgba(255, 255, 255, 0.65);
+    margin-left: 9rem;
+  }
 
-    
-    
-`
+  span {
+    margin-left: 10rem;
+    font-weight: 300;
+  }
+`;
 
 export const SocialButtons = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
+  button {
+    background: none;
+    border: 1px solid ${({ theme }) => theme.colors["three-white"]};
+    width: 3.125rem;
+    height: 3.125rem;
+    color: ${({ theme }) => theme.colors["base-white"]};
+    transition: 0.4s ease;
 
-    button{
-        background: none;
-        border: 1px solid ${({theme}) => theme.colors["three-white"]};
-        width: 3.125rem;
-        height: 3.125rem;
-        color: ${({theme }) => theme.colors["base-white"]};
-        transition: .4s ease;
-        
-        &:hover{
-            filter: brightness(.2);
-        }
-   
-
-
+    &:hover {
+      filter: brightness(0.2);
     }
-`
-
-
-
-
+  }
+`;
